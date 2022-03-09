0a6fbf39d5e0a296ec092b201231184b3c32bfe5
ericadiasdematos <erica.diasmatos99@gmail.com>
2022-03-09 11:31:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import "antd/dist/antd.css";
+import { Layout, Card, Button } from "antd";
+import { HeartFilled, ShoppingFilled } from "@ant-design/icons"
+// import { Link } from "react-router-dom";
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
+  const [booksList, setBooksList] = useState([]);
+  const [wishlist, setWishlist] = useState([])
+
+  useEffect(async () => {
+    fetch(
+      "https://api.nytimes.com/svc/books/v3/lists.json?list_name=Hardcover Advice&api-key=RmSeqDHFjqh09NAu7zxdTkOtK064g0tK"
+    )
+      .then((res) => res.json())
+      .then((json) => setBooksList(json.results));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <Layout style={layout}>
+          <Header style={header}>
+            <span style={titre}>New York Time's Bestseller Advice Books</span>
+          </Header>
+          <Content style={content}>
+            <div style={wishlistDiv}>
+              <a href="#wishlist" style={wishlist}>My Wishlist <HeartFilled style={styleHeart} /></a>
+            </div>
+            <div style={cardsDiv}>
+              {booksList.map(function (book, i) {
+                return (
+                  <Card
+                    key={i}
+                    title={book.book_details[0].title}
+                    extra={<div><ShoppingFilled style={iconStyleShop}/><HeartFilled style={iconStyleHeart}/></div>}
+                    style={{
+                      width: 300,
+                      minHeight: 250,
+                      textAlign: "start",
+                      marginBottom: 10,
+                      marginRight: 10,
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                    // actions={[
+                    //   <SettingOutlined key="setting" />,
+                    //   <EditOutlined key="edit" />,
+                    //   <EllipsisOutlined key="ellipsis" />,
+                    // ]}
+                  >
+                    <div style={divInCard}>
+                      <span style={authorStyle}>By: {book.book_details[0].author}</span>
+                      <span>{book.book_details[0].description}</span>
+                    </div>
+                  </Card>
+                );
+              })}
+            </div>
+          </Content>
+        </Layout>
+      </section>
+      <section id="wishlist">
+        <Layout style={layoutWishlist}>
+          <Header style={header}>
+              <span style={titre}>My Wishlist</span>
+          </Header>
+          <Content style={content}>
+
+          </Content>
+        </Layout>
+      </section>
     </div>
   );
+}
+
+let layout = {
+  height: "auto",
+};
+
+let layoutWishlist = {
+  height: "100vh",
+};
+
+let content = {
+  backgroundColor: "#EDF6F9",
+  display: "flex",
+  flexDirection: "column"
+};
+
+let header = {
+  backgroundColor: "#E29578",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+let footer = {
+  backgroundColor: "#83C5BE",
+};
+
+let cardsDiv = {
+  marginTop: 20,
+  marginBottom: 20,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minWidth: "100%",
+  flexWrap: "wrap",
+};
+
+let wishlistDiv = {
+  height: 100,
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+}
+
+let wishlist = {
+  fontSize: 20,
+  color: "#EDF6F9",
+  backgroundColor: "#83C5BE",
+  padding: 12,
+  borderRadius: 10,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+}
+
+let titre = {
+  color: "#EDF6F9",
+  fontSize: 30,
+  fontWeight: "bold"
+}
+
+let divInCard = {
+  display: "flex",
+  flexDirection: "column",
+  height: "100%"
+}
+
+let iconStyleShop = {
+  fontSize: 30,
+  marginRight: 20,
+  color: "#006D77",
+  cursor: "pointer"
+}
+
+let iconStyleHeart = {
+  fontSize: 30,
+  color: "#006D77",
+  cursor: "pointer"
+
+}
+
+let styleHeart = {
+  marginLeft: 7
+}
+
+let authorStyle = {
+  marginBottom: 15
 }
 
 export default App;
